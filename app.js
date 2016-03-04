19dc84d9db8c3998651530939ef19238d9bad373
Radhika Mattoo <rm3485@nyu.edu>
2016-03-04 18:29:37 -0500

    finished parts 2 and 3

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 // app.js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require("path");
+var port = 3000;
+
+
+//array of complaint objects
+var complaints = [
+	{line: "G", complaint: "The person sitting next to me was eating hard-boiled eggs in the subway car (???!!!)"},
+	{line: "F", complaint: "There was a possum loose on the platform"},
+	{line: "A", complaint: "The train was an hour late!"}
+];
+
+//layout
+app.set('view engine', 'hbs');
+
+//static files
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+//access request body
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(function(req, res, next){
+	console.log("Request method: " + req.method);
+	console.log("Request path: " + req.path);
+	console.log("Request query: ");console.log(req.query);
+	console.log("Request body: "); console.log(req.body);
+	next();
+});
+
+
+app.get("/", function(req, res){
+	//copy array by value and reverse
+	var filter;
+	var tempComplaints;
+	if(req.query.chosenLine !== undefined){ //form submitted?
+		filter = req.query.chosenLine;
+		tempComplaints = complaints.filter(function(c){
+			return c.line === filter;
+		});
+	}else{
+		tempComplaints = complaints.slice();
+	}
+	res.render('homepage.hbs', {'complaints': tempComplaints.reverse()});
+});
+
+app.listen(port);
+console.log("Hosting at port " + port);
